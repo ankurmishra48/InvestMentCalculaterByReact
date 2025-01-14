e3b07667574d63776c13d6a50f8b24b09516edee
ANKUR MISHRA <ankur25122@gmail.com>
2025-01-15 02:28:08 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import Header from "./components/Header"
+import UserInput from "./components/UserInput"
+import { useState } from "react"
+import Results from "./components/Results"
+import "./index.css"
+function App() {
+  function handleChange(inputidentifier,newvalue){
+    setuserInput(prevUserInput=>{
+        return{
+            ...prevUserInput,
+            [inputidentifier]: +newvalue
+        }
+    })
+
+  }
+
+  const [userInput,setuserInput]= useState({
+    initialInvestment:12000,
+    annualInvestment:1200,
+    expectedReturn:6,
+    duration:6
+
+  })
+const inputIsvalid =userInput.duration>=1
+  return (
+    <> 
+ <Header/>
+<UserInput userInput={userInput}
+onChange={handleChange}
+/>
+{!inputIsvalid && <p className="center">Please Enter valid Duration</p>}
+{inputIsvalid && <Results input={userInput} />}
+ </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import logo from "../assets/investment-calculator-logo.png"
+
+export default function Header(){
+return(
+    <header id="header">
+    <img src={logo} alt="wrong" />
+    <h1>Investment Calculater</h1>
+    </header>
+
+)
+
+}
